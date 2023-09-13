8a5b4c14e5cd3fe4d20b3100d7575110b7bc32f8
SyTHH <SyTHH@example.com>
2023-09-13 14:28:20 +0700

    Create DB

---
+++ DB_NodeProject.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE node_report;
+USE node_report;
+
+CREATE TABLE drinks(
+	drinkID char(5) primary key, 
+    name NVARCHAR(100),
+    drunkness int
+);
+
+CREATE TABLE users(
+	userID INT primary key AUTO_INCREMENT,
+    username nvarchar(100),
+    password nvarchar(100)
+);
+
+-- drop table drinks
+
+INSERT INTO drinks values
+('DR001', 'Bloody Mary', 3),
+('DR002','Martini', 5),
+('DR003','Scotch', 10)
+;
+
+
+INSERT INTO users values
+('asd',3),
+('sy',7),
+('qwe',4),
+('zxc',8)
+;
+
+
+select * from drinks;
+
+DELETE FROM Drinks WHERE drinkID = 'DR212';
